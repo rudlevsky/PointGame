5cc5c9bcd5bd756598b8416cd6030783e961b738
Turchin Alexander <alexanderturchin1@gmail.com>
2018-10-26 15:17:53 +0300

    Add IDisposable.

--- FPointGame/FPointGame/Form1.Designer.cs
+++ FPointGame/FPointGame/Form1.Designer.cs
@@ -28,43 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.hunter = new System.Windows.Forms.Label();
-            this.subscriber = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // hunter
-            // 
-            this.hunter.BackColor = System.Drawing.Color.Red;
-            this.hunter.ForeColor = System.Drawing.Color.Transparent;
-            this.hunter.Location = new System.Drawing.Point(261, 206);
-            this.hunter.Name = "hunter";
-            this.hunter.Size = new System.Drawing.Size(50, 50);
-            this.hunter.TabIndex = 0;
-            // 
-            // subscriber
-            // 
-            this.subscriber.BackColor = System.Drawing.Color.Blue;
-            this.subscriber.ForeColor = System.Drawing.Color.Transparent;
-            this.subscriber.Location = new System.Drawing.Point(512, 206);
-            this.subscriber.Name = "subscriber";
-            this.subscriber.Size = new System.Drawing.Size(50, 50);
-            this.subscriber.TabIndex = 1;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(819, 486);
-            this.Controls.Add(this.subscriber);
-            this.Controls.Add(this.hunter);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "Form1";
-            this.ShowIcon = false;
-            this.Text = "PointGame";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.ResumeLayout(false);
+			this.hunter = new System.Windows.Forms.Label();
+			this.subscriber = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// hunter
+			// 
+			this.hunter.BackColor = System.Drawing.Color.Red;
+			this.hunter.ForeColor = System.Drawing.Color.Transparent;
+			this.hunter.Location = new System.Drawing.Point(196, 167);
+			this.hunter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.hunter.Name = "hunter";
+			this.hunter.Size = new System.Drawing.Size(38, 41);
+			this.hunter.TabIndex = 0;
+			// 
+			// subscriber
+			// 
+			this.subscriber.BackColor = System.Drawing.Color.Blue;
+			this.subscriber.ForeColor = System.Drawing.Color.Transparent;
+			this.subscriber.Location = new System.Drawing.Point(384, 167);
+			this.subscriber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.subscriber.Name = "subscriber";
+			this.subscriber.Size = new System.Drawing.Size(38, 41);
+			this.subscriber.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(614, 395);
+			this.Controls.Add(this.subscriber);
+			this.Controls.Add(this.hunter);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "PointGame";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.ResumeLayout(false);
 
         }
 
--- FPointGame/FPointGame/GenericTypes/Message.cs
+++ FPointGame/FPointGame/GenericTypes/Message.cs
@@ -10,16 +10,45 @@ namespace FPointGame.GenericTypes
     /// Minimal realization which message should have.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class Message<TContent>
+    public class Message<TContent> : IDisposable
     {
-        /// <summary>
-        /// A container which has our message data.
-        /// </summary>
-        public TContent Content { get; }
+		private bool _isDisposed;
+
+		/// <summary>
+		/// A container which has our message data.
+		/// </summary>
+		public TContent Content { get; }
 
         public Message(TContent data)
         {
             Content = data;
         }
+
+		/// <summary>
+		/// Dipose the <see cref="Message{TContent}"/>.
+		/// </summary>
+		public void Dispose()
+		{
+			this.Dispose(true);
+		}
+
+		/// <summary>
+		/// The logic of disposing the <see cref="Message{TContent}"/>.
+		/// </summary>
+		/// <param name="flag">
+		/// <c>true</c> if is called from <see cref="Dispose"/>.
+		/// <c>false</c> if from <see cref="GC"/>.
+		/// </param>
+		protected virtual void Dispose(bool flag)
+		{
+			if (_isDisposed) return;
+			if (flag) GC.SuppressFinalize(this);
+			this._isDisposed = true;
+		}
+
+		~Message()
+		{
+			Dispose(false);
+		}
     }
 }
